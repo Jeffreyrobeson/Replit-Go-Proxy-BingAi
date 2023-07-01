402556a88c695a3ebe6dd57b8e2bc573cd8cf17a
Jeffreyrobeson <132809848+Jeffreyrobeson@users.noreply.github.com>
2023-07-02 02:19:37 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ if [ ! -f "go-proxy-bingai" ];then
   echo Replit一键部署Go-Proxy-BingAI
   echo By The 一只鬆
   echo 仓库开源地址：https://github.com/Jeffreyrobeson/Replit-Go-Proxy-BingAi
-  curl -L https://github.com/Jeffreyrobeson/go-proxy-vbingai/archive/refs/tags/v1.8.7.tar.gz -o bingai.tar.gz
+  curl -L https://github.com/adams549659584/go-proxy-bingai/releases/latest/download/go-proxy-bingai-linux-amd64.tar.gz -o bingai.tar.gz
   tar -zxvf bingai.tar.gz
   rm -f bingai.tar.gz
   echo 搭建完成，单击Run以启动项目
@@ -11,4 +11,4 @@ fi
 # 防休眠命令
 while true; do curl -s --user-agent "${UA_Browser}" "https://${REPL_SLUG}.${REPL_OWNER}.repl.co" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 600; done &
 # 启动BingAI
-./go-proxy-vbingai-1.8.7
+./go-proxy-vbingai
