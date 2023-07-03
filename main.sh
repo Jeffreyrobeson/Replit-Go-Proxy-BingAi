2efdb5e88b471d8bd1c80900a90b1f25527df31d
jeffreyrobeson <132809848+jeffreyrobeson@users.noreply.github.com>
2023-07-03 18:26:23 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ if [ ! -f "go-proxy-bingai" ];then
   echo Replit一键部署Go-Proxy-BingAI
   echo By The 一只鬆
   echo 仓库开源地址：https://github.com/Jeffreyrobeson/Replit-Go-Proxy-BingAi
-  curl -L https://github.com/adams549659584/go-proxy-bingai/releases/latest/download/go-proxy-bingai-linux-amd64.tar.gz -o bingai.tar.gz
+  curl -L https://github.com/jeffreyrobeson/go-proxy-vbingai/releases/download/go-proxy-vbingai/go-proxy-bingai-linux-amd64.tar.gz -o bingai.tar.gz
   tar -zxvf bingai.tar.gz
   rm -f bingai.tar.gz
   echo 搭建完成，单击Run以启动项目
